5815d0a2dcfbb98ebb3bfa449709fba7f98bfd32
laurelton <777443+laurelton@users.noreply.github.com>
2018-04-02 16:07:47 -0400

    Completed Jasmine test, begun Objects test

--- koans/jasmine/spec/1-game-of-life.js
+++ koans/jasmine/spec/1-game-of-life.js
@@ -1,13 +1,25 @@
 /* global isCellAliveInNextGeneration */
-describe('Game of Life', function () {
-  describe('Determining whether cell is alive in next generation', function () {
-    it('should return false when a live cell has fewer than two live neighbours - under-population', function () {
+describe('Game of Life', function() {
+    describe('Determining whether cell is alive in next generation', function() {
+        it('should return false when a live cell has fewer than two live neighbours - under-population', function() {
+            let actual1 = isCellAliveInNextGeneration(true, 1);
+            let actual0 = isCellAliveInNextGeneration(true, 0);
+            expect(actual0).toBe(false);
+            expect(actual1).toBe(false);
+        });
+        it('should return true when a live cell has two or three live neighbours - survival', function() {
+            let actual2 = isCellAliveInNextGeneration(true, 2);
+            let actual3 = isCellAliveInNextGeneration(true, 3);
+            expect(actual2).toBe(true);
+            expect(actual3).toBe(true);
+        });
+        it('should return false when a live cell has more than three live neighbours - overcrowding', function() {
+            let actual = isCellAliveInNextGeneration(true, 4);
+            expect(actual).toBe(false);
+        });
+        it('should return true when a dead cell has exactly three live neighbours - reproduction', function() {
+            let actual = isCellAliveInNextGeneration(false, 3);
+            expect(actual).toBe(true);
+        });
     });
-    it('should return true when a live cell has two or three live neighbours - survival', function () {
-    });
-    it('should return false when a live cell has more than three live neighbours - overcrowding', function () {
-    });
-    it('should return true when a dead cell has exactly three live neighbours - reproduction', function () {
-    });
-  });
 });
--- koans/jasmine/src/1-game-of-life.js
+++ koans/jasmine/src/1-game-of-life.js
@@ -1,2 +1,13 @@
 function isCellAliveInNextGeneration(isCellAlive, numberOfLiveNeighbours) {
+    if (numberOfLiveNeighbours === 2) {
+        return isCellAlive;
+    }
+
+    if (numberOfLiveNeighbours === 3) {
+        return true;
+    }
+
+    if (numberOfLiveNeighbours < 2 || numberOfLiveNeighbours > 3) {
+        return false;
+    }
 }
